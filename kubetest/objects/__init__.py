2af1e140b0ced56eca2dc4b901f2bd2f4864089d
Thunder Shiviah <7097428+ThunderShiviah@users.noreply.github.com>
2022-04-07 23:10:49 +0200

    Update __init__.py

--- kubetest/objects/__init__.py
+++ kubetest/objects/__init__.py
@@ -3,7 +3,7 @@
 # flake8: noqa
 
 from .api_object import ApiObject
-from .clusterrolebinding import ClusterRoleBinding
+#from .clusterrolebinding import ClusterRoleBinding
 from .configmap import ConfigMap
 from .container import Container
 from .daemonset import DaemonSet
@@ -16,8 +16,8 @@ from .node import Node
 from .persistentvolumeclaim import PersistentVolumeClaim
 from .pod import Pod
 from .replicaset import ReplicaSet
-from .rolebinding import RoleBinding
+#from .rolebinding import RoleBinding
 from .secret import Secret
-from .service import Service
-from .serviceaccount import ServiceAccount
+#from .service import Service
+#from .serviceaccount import ServiceAccount
 from .statefulset import StatefulSet
